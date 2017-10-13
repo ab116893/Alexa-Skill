0642ddf4ebe50640f2ed4a5909755fc9829d042f
ab2010 <ab116893@gmail.com>
2017-10-13 16:57:31 -0400

    some change to the skill

--- Alexa_Skill.py
+++ Alexa_Skill.py
@@ -4,24 +4,72 @@ Created on Mon Oct  9 20:03:14 2017
 
 @author: sudar
 """
-
+import urllib
 from bs4 import BeautifulSoup
 import json
-import requests
+import urllib.request
+        
+
+def make_soup(url):
+        link = urllib.request.urlopen(url)
+        soupdata = BeautifulSoup(link, "html.parser")
+        return soupdata
+soup = make_soup("https://www.unh.edu/dining/regular-hours-operation")
+
+
+    
+    #for hours in record.find_all("th"):
+   #hoursdata = ""
+mylist =[]
+for record in soup.find_all("tr"):
+    list_of_hours=[]
+    for hour in record.find_all(["th","td"]):
+        getdata=record.text, hour.text
+        list_of_hours.append(getdata)
+    mylist.append(list_of_hours)
+print(mylist)
+
+        
+        
+    
+ 
+            
+        
+ 
+    
+            
+                
+                
+            
+            
+            
+            
+            
+           
+            
+       
+      
+       
+       
+       
+       
+        #hoursdata =hoursdata+","+data.text
+        
+        #hoursdatasave = hoursdatasave + "\n" + hoursdata[1:]
+
+    
+
+    
+        
+    
+    
 
 
 
-url = requests.get("https://www.unh.edu/dining/regular-hours-operation")
-html_content = url.text
-soup = BeautifulSoup(url.content,"html.parser")
-for tr in soup.find_all("tr")[1]:
-    tds = tr.find_all("td")
-    print("value:%s, value2:%s, value3:%s\n" % \
-          (tds[0].text, tds[0].text, tds[2].text))
+
     
-    
-    
-    
+
+
     
 
 
